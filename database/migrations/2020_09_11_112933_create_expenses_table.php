549061fbf83e407063ea6e72e2e7172c8f3dd629
MrWilllian <willian.dev10@gmail.com>
2020-09-11 21:44:01 -0300

    refactor: add Expense migration

---
+++ database/migrations/2020_09_11_112933_create_expenses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('account_id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamp('date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->decimal('value')->default(0);
+
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
