16e1bfb51f451eec79cfbedb39d679290a2df1bd
MrWilllian <willian.dev10@gmail.com>
2020-09-17 10:31:59 -0300

    refactor: cleaning controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,41 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\User;
+use App\Services\TokenService;
+use App\Http\Repositories\UserRepository;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-      $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'tellphone' => $request->tellphone,
-      ]);
+  protected $users;
 
-      $token = auth()->login($user);
+  public function __construct(UserRepository $users)
+  {
+    $this->users = $users;
+  }
 
-      return $this->respondWithToken($token);
-    }
+  public function register(Request $request)
+  {
+    $user = $this->users->create($request);
+    return $this->respondWithToken(TokenService::loginAndGetToken($user));
+  }
 
-    public function login(Request $request)
-    {
-      $credentials = $request->only(['email']);
-      $user = User::where('email', $credentials)->first();
+  public function login(Request $request)
+  {
+    $credentials = $request->only(['email']);
+    $user = User::where('email', $credentials)->first();
 
-      if (!$user) 
-        return response()->json(['error' => 'Unauthorized'], 401);
+    if (!$user) 
+      return response()->json(['error' => 'User not found'], 404);
 
-      $token = auth()->login($user);
-      return $this->respondWithToken($token);
-    }
+    //$user->notify(new VerificationCode('Code Here'));
+    //$this->sendMail();
 
-    protected function respondWithToken($token)
-    {
-      return response()->json([
-        'access_token' => $token,
-        'token_type' => 'bearer',
-        'expires_in' => auth()->factory()->getTTL() * 60
-      ]);
-    }
+    return $this->respondWithToken(TokenService::loginAndGetToken($user));
+  }
+
+  protected function sendMail() {
+    $to_name = 'Willian';
+    $to_email = 'williansoares102@gmail.com';
+    $data = array(
+      'name' => "Usuário", 
+      'body' => "Parabéns, você entrou no Finances App!"
+    );
+
+    Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) {
+      $message->to($to_email, $to_name)->subject('Seja Bem-vindo!');
+      $message->from('willian.dev10@gmail.com', 'Test Mail');
+    });
+  }
+
+  protected function respondWithToken($token)
+  {
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'bearer',
+      'expires_in' => auth()->factory()->getTTL() * 60
+    ]);
+  }
 }
