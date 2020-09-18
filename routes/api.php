734142334f1efd8da9b880b89020151a3a35eeea
MrWilllian <willian.dev10@gmail.com>
2020-09-18 10:02:08 -0300

    refactor: include logout route

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
-
-Route::post('sendSMS', 'SMSController@sendSMS');
+Route::get('logout', 'AuthController@logout');
