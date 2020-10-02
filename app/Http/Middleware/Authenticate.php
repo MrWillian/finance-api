c3227ff23d1b7d6ffc5770b88798ae11716f51a0
MrWilllian <willian.dev10@gmail.com>
2020-10-01 21:07:30 -0300

    refactor: implement unauthenticated method

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,9 +3,24 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use App\Exceptions\UnauthenticatedException;
 
 class Authenticate extends Middleware
 {
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \App\Exceptions\UnauthenticatedException
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        throw new UnauthenticatedException();
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -17,5 +32,5 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
-    }
+    }    
 }
