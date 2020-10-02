3abc39b84c94a1e92a3172f712e94aa8400f99d0
MrWilllian <willian.dev10@gmail.com>
2020-10-01 21:12:50 -0300

    refactor: register TokenService bind

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
+use Aws\Sns\SnsClient;
+use Aws\Credentials\Credentials;
+use App\Channels\SmsChannel;
 use App\User;
 use App\Observers\UserObserver;
+use App\Services\TokenService;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Notifications\ChannelManager;
+use Illuminate\Support\Facades\Notification;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +21,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
+        $this->app->bind(TokenService::class, function() {
+            return new TokenService();
+        });
+
+        Notification::resolved(function (ChannelManager $service) {
+            $service->extend('sms', function ($app) {
+                return new SmsChannel(
+                    new SnsClient([
+                        'version' => '2010-03-31',
+                        'credentials' => new Credentials(
+                            config('services.sns.key'),
+                            config('services.sns.secret')
+                        ),
+                        'region' => config('services.sns.region'),
+                    ])
+                );
+            });
+        });
     }
 
     /**
