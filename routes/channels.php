3bc6d44881404349b2c2840a44e2a10b592ad42b
MrWilllian <willian.dev10@gmail.com>
2020-10-07 08:33:42 -0300

    refactor: include account channel authorization

--- routes/channels.php
+++ routes/channels.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+use App\Account;
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('account.{accountId}', function ($user, $accountId) {
+    return $user->id === Account::findOrNew($accountId)->user_id;
 });
