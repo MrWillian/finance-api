c9352c0d786f784a6ba4a566dec3c46113d7d89a
MrWilllian <willian.dev10@gmail.com>
2020-11-18 15:18:26 -0300

    refactor: include Settings Repository

---
+++ app/Http/Repositories/SettingsRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Settings;
+use App\Http\Resources\SettingsResource;
+use App\Http\Repositories\ApiRepository;
+use App\Exceptions\FieldValidatorException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SettingsRepository extends ApiRepository {
+  protected $modelClass = Settings::class;
+
+  public function index() {
+    return $this->successResponse(SettingsResource::collection(Settings::all()));
+  }
+
+  public function getSettingsForUser($request) {
+    try {
+      $query = $this->newQuery();
+      $query->where('user_id', $request->user()->id)->orderBy('id');
+      return $this->successResponse($this->doQuery($query), 200);
+    } catch(Exception $exception) {
+      return $this->errorResponse($exception, 500);
+    }
+  }
+}
