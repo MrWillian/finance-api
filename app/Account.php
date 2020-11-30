ab4442423cf7f66e78a85e0408d7fa3d0b917e9a
MrWilllian <willian.dev10@gmail.com>
2020-11-30 10:37:27 -0300

    refactor: set Accessors to name, description and amount

--- app/Account.php
+++ app/Account.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Account extends Model
 {
     protected $fillable = ['name', 'description', 'amount', 'user_id'];
+    
+    public function getNameAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
+
+    public function getDescriptionAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
+
+    public function getAmountAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 }
